c8990c6c3fd289f764a16388025e966c7a450fc0
Арина Богомольная <bogomolnayaarina@gmail.com>
2025-03-19 13:24:17 +0300

    burger_constructor

--- first_level/start_test.gd
+++ first_level/start_test.gd
@@ -1,9 +1,9 @@
 extends Button
 @onready var start_test: Button = $"."
 @onready var _1_test_box: VBoxContainer = $"../1 test box"
-@onready var next: Button = $"../1 test box/HBoxContainer/Next"
+@onready var next: Button = $"../Next"
 
 func _pressed():
 	start_test.visible = false
 	_1_test_box.visible = true
-	#next.visible = true
+	next.visible = true
--- first_level/описание.gd
+++ first_level/описание.gd
@@ -13,31 +13,12 @@ extends TabBar
 @onready var _1_2: Button = $"1 test box/1_2"
 @onready var _1_3: Button = $"1 test box/1_3"
 
-var questions = [
-	'Какая булочка?',
-	'Из какого мяса котлета?',
-	'Какой будет соус?'
-]
-var answers_1 = [
-	'Кунжутная',
-	'Свинина',
-	'Кетчуп'
-]
-var answers_2 = [
-	'Бородинская',
-	'Говядина',
-	'Барбекю'
-]
-var answers_3 = [
-	'Кукурузная',
-	'Индейка',
-	'Горчица'
-]
-var current_index = 0
+
+var answ_1 = ''
+var answ_2 = ''
+var answ_3 = ''
 
 func _ready() -> void:
-	update_question()
-	update_buttons()
 	start_test.visible = false
 	back.visible = false
 	next.visible = false
@@ -46,24 +27,7 @@ func _ready() -> void:
 	_3_test_box.visible = false
 
 
-func update_question():
-	_1_question.text = questions[current_index]
-	_1_1.text = answers_1[current_index]
-	_1_2.text = answers_2[current_index]
-	_1_3.text = answers_3[current_index]
 
-
-func update_buttons():
-	back.disabled = current_index == 0
-	next.disabled = current_index == questions.size() - 1
-	
-	
-func _on_next_button_pressed():
-	if current_index < questions.size() - 1:
-		current_index += 1
-		update_question()
-		update_buttons()
-	
-#func _process(delta: float) -> void:
-#	descr_list.text = 'Булочка: ' + answ_1 + '\n' + 'Котлета: ' + answ_2 + '\n' + 'Соус: ' + answ_3
+func _process(delta: float) -> void:
+	descr_list.text = 'Булочка: ' + answ_1 + '\n' + 'Котлета: ' + answ_2 + '\n' + 'Соус: ' + answ_3
 	
